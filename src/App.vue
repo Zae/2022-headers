e68925155134e21aa5fc3c531bafdeab5737942f
Ezra Pool <ezra@digitalnatives.nl>
2022-05-05 14:49:51 +0200

    make it big!

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <template>
-    <div class="first">
-        <h1><HeaderAutomata>Lorem <span>ipsum</span></HeaderAutomata></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nullam eget felis eget nunc. Mollis aliquam ut porttitor leo a. Sit amet dictum sit amet justo donec. </p>
-    </div>
-    <div class="second">
-        <h1><HeaderAutomata>Lorem ipsum</HeaderAutomata></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nullam eget felis eget nunc. Mollis aliquam ut porttitor leo a. Sit amet dictum sit amet justo donec. </p>
-    </div>
     <div class="third">
-        <h1><HeaderAutomata>Lorem ipsum</HeaderAutomata></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nullam eget felis eget nunc. Mollis aliquam ut porttitor leo a. Sit amet dictum sit amet justo donec. </p>
-    </div>
-    <div class="fourth">
-        <h1><HeaderAutomata>Lorem ipsum</HeaderAutomata></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nullam eget felis eget nunc. Mollis aliquam ut porttitor leo a. Sit amet dictum sit amet justo donec. </p>
-    </div>
-    <div class="fifth">
-        <h1><HeaderAutomata>Lorem ipsum</HeaderAutomata></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nullam eget felis eget nunc. Mollis aliquam ut porttitor leo a. Sit amet dictum sit amet justo donec. </p>
+        <h1>
+            <HeaderAutomata>CREATIVE</HeaderAutomata> <br/>
+            <HeaderAutomata>CODING</HeaderAutomata>
+        </h1>
     </div>
 </template>
 
@@ -32,20 +18,14 @@ export default {
 </script>
 
 <style>
-h1 { font-size: 6em; }
+h1 { font-size: 9em; }
 
-.first, .second, .third, .fourth, .fifth {
+.third {
     font-size: 2em;
     line-height: 1;
 }
 
-.first { font-family: Helvetica, serif; }
-.second { font-family: "Times New Roman", serif; }
-.third { font-family: 'Noto Serif Display', serif; }
-.fourth { font-family: 'Square Peg', cursive; }
-.fifth { font-family: 'Tapestry', cursive; }
-
 h1, h2, h3, h4, h5, p {
-    margin: 10px 0;
+    margin: 10px 10px;
 }
 </style>
